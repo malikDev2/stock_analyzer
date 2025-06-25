9a0426ad68f6490a0d6527a95ce0b71cb90d6d9e
Malik <malik.h.jacobs@outlook.com>
2025-06-24 22:33:49 -0400

    created layout for react page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,26 +8,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Stock report</h1>
+      <h2>Categories:</h2>
+      <ul>
+        <li>Market cap timeline</li>
+        <li>Stocks by presidency</li>
+        <li>Technology trends</li>
+      </ul>
     </>
   )
 }
