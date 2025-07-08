e1126090d3d8b964b3006f5191ea868f49c4da52
Malik <malik.h.jacobs@outlook.com>
2025-07-08 12:44:40 -0400

    created all trend divs

--- my-app/src/pages/trends.tsx
+++ my-app/src/pages/trends.tsx
@@ -10,7 +10,8 @@ import valgrcom2 from "../assets/valgrcom2.png";
 import valgrcom3  from "../assets/valgrcom3.png";
 import valgrdig  from "../assets/valgrdig.png";
 import valgrdig2  from "../assets/valgrdig2.png";
-import valgrten  from "../assets/valgrten2.png";
+import valgrten  from "../assets/valgrten.png";
+import valgrten2  from "../assets/valgrten2.png";
 
 
 const Trends = () => {
@@ -26,6 +27,93 @@ const Trends = () => {
     //   backgroundColor: "",
     //   pageBackgroundColor: "",
     // }
+    {
+        header: "Market Cap of Highest Three Valued Companies Today",
+        imageSrc: mcthree,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech ETF Price as % of S&P 500 Price",
+        imageSrc: etfsp,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech ETF Value Growth Relative to S&P 500",
+        imageSrc: etfsp2,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech ETF Value Growth Relative to S&P 500 (Since .com Burst)",
+        imageSrc: etfsp3,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth During Computing Era",
+        imageSrc: valgrcom2,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth During Computing Era (Without Microsoft/Intel)",
+        imageSrc: valgrcom3,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth During .Com Era",
+        imageSrc: valgrcom,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth During Digital Era",
+        imageSrc: valgrdig,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth During Digital Era (Without Netflix)",
+        imageSrc: valgrdig2,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth During AI Era",
+        imageSrc: valgrai,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth Through Past 10 Years",
+        imageSrc: valgrten,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    {
+        header: "Tech Value Growth Through Past 10 Years (Without Nvidia)",
+        imageSrc: valgrten2,
+        caption: "",
+        backgroundColor: "rgb(147, 184, 247)",
+        pageBackgroundColor: "rgb(26, 5, 255)",
+    },
+    
+    
+
   ];
 
   useEffect(() => {
