3d1656e18c46b2ad3899508a1347bad38633b2b9
Malik <malik.h.jacobs@outlook.com>
2025-07-10 18:41:01 -0400

    added images to homedivs

--- my-app/src/pages/home.tsx
+++ my-app/src/pages/home.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import time from "../assets/time.png";
+import trend from "../assets/trend.png";
+import pres from "../assets/pres.png";
 
 const Home = () => {
     return(<>
@@ -8,18 +11,21 @@ const Home = () => {
       <Link to="/timeline" className="L">
         <div className="homediv">
           <h1>Timeline</h1>
+          <img src={time} alt="timeline" />
           <p>View the growth in market cap of Tech companies over the years</p>
+          
         </div>
       </Link>
       <Link to="/presidencies" className="L">
         <div className="homediv" style={{ backgroundColor: 'rgba(60, 122, 255, 0.99)' }}>
           <h1>Presidencies</h1>
-          
+          <img src={pres} alt="presidents" />
         </div>
       </Link>
       <Link to="/trends" className="L">
         <div className="homediv" style={{ backgroundColor: 'rgba(180, 0, 197, 0.79)' }} >
           <h1>Trends</h1>
+          <img src={trend} alt="trends" />
           
         </div>
       </Link>
